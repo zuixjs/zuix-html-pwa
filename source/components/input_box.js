ade26a7ac3fb8d1eb62b2c4589daed6f8f1dce76
Generoso Martello <generoso.martello@gmail.com>
2018-08-09 16:54:44 +0200

    added pages: search, notifications, messages; side panel swipe gesture now working

---
+++ source/components/input_box.js
@@ -0,0 +1,4 @@
+'use strict';
+zuix.controller(function(cp) {
+
+});
--- source/index.js
+++ source/index.js
@@ -1,4 +1,10 @@
 /* global zuix */
+
+const PAGE_HOME = 0;
+const PAGE_SEARCH = 1;
+const PAGE_NOTIFICATIONS = 2;
+const PAGE_MESSAGES = 3;
+
 let drawerLayout;
 let viewPager;
 let topicIndicator;
@@ -9,90 +15,106 @@ zuix.using('style', '//genielabs.github.io/zkit/css/flex-layout-attribute.min.cs
 zuix.using('style', './index.css');
 
 zuix.$.find('.profile').on('click', function() {
-  if (drawerLayout) drawerLayout.open();
+    if (drawerLayout) drawerLayout.open();
 });
 
 window.options = {
-  drawerLayout: {
-    autoHideWidth: -1,
-    drawerWidth: 280,
-    ready: function() { drawerLayout = this; this.close(); }
-  },
-  viewPager: {
-    enablePaging: true,
-    on: {
-      'page:change': function(e, page) {
-        syncPageIndicator(page);
-        // show header/footer
-        if (viewPager) {
-          const p = viewPager.get(page.in);
-          zuix.context(p).show();
-        }
-      }
+    drawerLayout: {
+        autoHideWidth: -1,
+        drawerWidth: 280,
+        ready: function() { drawerLayout = this; this.close(); }
     },
-    ready: function() {
-      viewPager = this;
+    footerBar: {
+        ready: function(){
+            const view = zuix.$(this.view());
+            const buttons = view.find('button');
+            buttons.each(function(i, el) {
+                // TODO:
+                this.on('click', function() {
+                    buttons.removeClass('active');
+                    this.addClass('active');
+                    showPage(i);
+                });
+            });
+        }
+    },
+    viewPager: {
+        enablePaging: true,
+        on: {
+            'page:change': function(e, page) {
+                syncPageIndicator(page);
+                // show header/footer
+                if (viewPager) {
+                    const p = viewPager.get(page.in);
+                    zuix.context(p).show();
+                }
+            }
+        },
+        ready: function() {
+            viewPager = this;
+        }
+    },
+    topicIndicator: {
+        enablePaging: true,
+        ready: function() {
+            topicIndicator = this;
+        }
+    },
+    autoHidingBars: {
+        header: 'header-bar',
+        footer: 'footer-bar',
+        height: 56,
+        on: {
+            'page:scroll': function(e, data) {
+                zuix.componentize();
+            }
+        }
+    },
+    content: {
+        css: false
     }
-  },
-  topicIndicator: {
-    enablePaging: true,
-    ready: function() {
-      topicIndicator = this;
-    }
-  },
-  autoHidingBars: {
-    header: 'header-bar',
-    footer: 'footer-bar',
-    height: 56,
-    on: {
-      'page:scroll': function(e, data) {
-        zuix.componentize();
-      }
-    }
-  },
-  content: {
-    css: false
-  }
 };
 
 // Callback when the 'header' context is ready,
 // that means that the 'layout/header' was loaded
 zuix.context('header', function() {
-  const view = zuix.$(this.view());
-  // handle 'topic' buttons click (goto clicked topic page)
-  topicButtons = view.find('.topics').children().each(function(i, el){
-    this.on('click', function(e) {
-      if (viewPager) viewPager.page(i);
+    const view = zuix.$(this.view());
+    // handle 'topic' buttons click (goto clicked topic page)
+    topicButtons = view.find('.topics').children().each(function(i, el){
+        this.on('click', function(e) {
+            if (viewPager) viewPager.page(i);
+        });
     });
-  });
-  // open drawer when the profile icon is clicked
-  view.find('.profile').on('click', function() {
-    if (drawerLayout) drawerLayout.open();
-  });
+    // open drawer when the profile icon is clicked
+    view.find('.profile').on('click', function() {
+        if (drawerLayout) drawerLayout.open();
+    });
+    showPage(PAGE_HOME);
 });
 
 function syncPageIndicator(page) {
-  if (topicButtons) {
-    topicButtons.eq(page.out).removeClass('active');
-    topicButtons.eq(page.in).addClass('active');
-  }
-  if (topicIndicator) topicIndicator.page(page.in);
+    if (topicButtons) {
+        topicButtons.eq(page.out).removeClass('active');
+        topicButtons.eq(page.in).addClass('active');
+    }
+    if (topicIndicator) topicIndicator.page(page.in);
 }
 
-/*
-// set @lib to load components from my laptop ( Dev mode ON =) )
-zuix.store("config", {
-  "libraryPath": "http://192.168.2.104:8080/lib/"
-});
-*/
+function showPage(i) {
+    // show header top-box
+    zuix.field('header-box')
+        .children().hide()
+        .eq(i).show();
+    // show header bottom-box
+    zuix.field('header-tools')
+        .children().hide()
+        .eq(i).show();
+    // show page
+    zuix.field('pages')
+        .children().hide()
+        .eq(i).show();
+    if (viewPager) viewPager.refresh();
+}
 
 // Turn off debug output
 window.zuixNoConsoleOutput = true;
-
-/*
-const width = window.innerWidth
-    || document.documentElement.clientWidth
-    || document.body.clientWidth;
-const w = -(width/2);
-zuix.lazyLoad(true, w);
-*/
