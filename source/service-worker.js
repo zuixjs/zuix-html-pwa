24a1b0a1e19d167b80117acf607d9661b8c20ad9
Generoso Martello <generoso.martello@gmail.com>
2018-08-09 17:09:11 +0200

    fix error logged to console

--- source/service-worker.js
+++ source/service-worker.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
+    const promise = navigator.serviceWorker
         .register('./sw.js')
         .then(reg => console.log('PWA service-worker ready.', reg))
         .catch(err => console.error('Could not load service-worker.', err));
