d965430595c0c467615b1eef1654fd5a96829896
Generoso Martello <generoso.martello@gmail.com>
2018-08-09 17:52:09 +0200

    fix TypeError after loading service-worker.js

--- docs/service-worker.js
+++
@@ -1,6 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('./sw.js')
-        .then(reg => console.log('PWA service-worker ready.', reg))
-        .catch(err => console.error('Could not load service-worker.', err));
-}
--- source/service-worker.js
+++ source/service-worker.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    const promise = navigator.serviceWorker
+    const p = navigator.serviceWorker
         .register('./sw.js')
         .then(reg => console.log('PWA service-worker ready.', reg))
         .catch(err => console.error('Could not load service-worker.', err));
