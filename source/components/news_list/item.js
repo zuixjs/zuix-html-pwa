f2fc944e6a33b2c5ad2a4f65d9a0609ede53d0ff
Generoso Martello <generoso.martello@gmail.com>
2018-08-09 23:33:40 +0200

    fix FireFox image drag (finally)

--- source/components/news_list/item.js
+++ source/components/news_list/item.js
@@ -2,7 +2,12 @@
 'use strict';
 zuix.controller(function(cp){
   cp.create = function() {
-
+    cp.field('container').css({
+      'background-image': 'url('+cp.model().cover+')',
+      'background-size': 'cover',
+      'background-repeat': 'no-repeat',
+      'background-position': 'center center'
+    });
     cp.view().on('click', function() {
       window.open(cp.model().link);
     });
